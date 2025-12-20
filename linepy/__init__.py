c10698d5179bad48d0adbbfbaeed293a8b6bed82
عبير <alqaws151@gmail.com>
2025-12-21 01:12:50 +0300

    Update __init__.py

--- linepy/__init__.py
+++ linepy/__init__.py
@@ -6,7 +6,7 @@ from akad.ttypes import OpType
 __copyright__       = 'Copyright 2018 by Fadhiil Rachman'
 __version__         = '3.0.8'
 __license__         = 'BSD-3-Clause'
-__author__          = 'Fadhiil Rachman'
+__author__          = 'Fadhiil Rachman - تعريب: نسخة عربية'
 __author_email__    = 'fadhiilrachman@gmail.com'
 __url__             = 'http://github.com/fadhiilrachman/line-py'
 
