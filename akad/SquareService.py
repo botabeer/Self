63e6b29db805dd61cbca4e4e1486c6833bdd21d6
عبير <alqaws151@gmail.com>
2025-12-21 17:34:03 +0300

    Update SquareService.py

--- akad/SquareService.py
+++ akad/SquareService.py
@@ -1,70 +1,178 @@
-# -*- coding: utf-8 -*-
-# تم توليد هذا الملف بواسطة Thrift Compiler
-# هذا الملف يدير خدمات LINE Square / OpenChat
-# تم تعريب التعليقات لشرح الوظائف الحساسة المتعلقة بالرقابة والحماية
-
-from thrift.Thrift import TType, TMessageType, TFrozenDict, TException, TApplicationException
+from thrift.Thrift import TType, TMessageType, TException, TApplicationException
 from thrift.protocol.TProtocol import TProtocolException
 from thrift.TRecursive import fix_spec
 import sys
 import logging
 from .ttypes import *
+from thrift.Thrift import TProcessor
+from thrift.transport import TTransport
+
+all_structs = []
 
 class Iface(object):
-    """
-    واجهة خدمات السكوير (Square Service Interface)
-    تحتوي على العمليات اللازمة لإدارة الأعضاء والمحتوى
-    """
-
-    def reportSquareMember(self, request):
-        """
-        الإبلاغ عن عضو:
-        تستخدم في أنظمة الحماية التلقائية للإبلاغ عن الحسابات المخربة.
-        """
-        pass
-
-    def deleteSquareChatAnnouncement(self, request):
-        """حذف إعلانات الشات في السكوير"""
-        pass
-
-    def createSquareChatAnnouncement(self, request):
-        """إنشاء إعلان جديد في الشات"""
-        pass
-
-    def getSquareStatus(self, request):
-        """
-        الحصول على حالة السكوير:
-        مفيد لمراقبة عدد الأعضاء وحالة الحماية بشكل لحظي.
-        """
-        pass
-
-    def destroyMessage(self, request):
-        """
-        تدمير (حذف) رسالة:
-        أمر حيوي جداً في "Content Protection" لحذف الروابط الممنوعة أو الرسائل المزعجة فوراً.
-        """
-        pass
-
-# ... (يستمر الملف في تعريف العمليات التقنية لإرسال واستقبال البيانات)
+    """واجهة خدمات القروبات والحماية"""
+    
+    # إدارة القروبات
+    def getSquare(self, request): pass
+    def getJoinedSquares(self, request): pass
+    def createSquare(self, request): pass
+    def updateSquare(self, request): pass
+    def deleteSquare(self, request): pass
+    def joinSquare(self, request): pass
+    def leaveSquare(self, request): pass
+    
+    # إدارة الأعضاء
+    def getSquareMembers(self, request): pass
+    def getSquareMember(self, request): pass
+    def updateSquareMember(self, request): pass
+    def updateSquareMembers(self, request): pass
+    def approveSquareMembers(self, request): pass
+    def rejectSquareMembers(self, request): pass
+    def searchSquareMembers(self, request): pass
+    
+    # الحماية والتبليغات
+    def reportSquare(self, request): pass
+    def reportSquareMember(self, request): pass
+    def reportSquareMessage(self, request): pass
+    def reportSquareChat(self, request): pass
+    
+    # صلاحيات الحماية
+    def getSquareAuthority(self, request): pass
+    def updateSquareAuthority(self, request): pass
+    def updateSquareMemberRelation(self, request): pass
+    
+    # إدارة المحادثات
+    def getSquareChat(self, request): pass
+    def getJoinedSquareChats(self, request): pass
+    def createSquareChat(self, request): pass
+    def updateSquareChat(self, request): pass
+    def deleteSquareChat(self, request): pass
+    def joinSquareChat(self, request): pass
+    def leaveSquareChat(self, request): pass
+    
+    # إدارة الرسائل
+    def sendMessage(self, request): pass
+    def destroyMessage(self, request): pass
+    def fetchSquareChatEvents(self, request): pass
+    def markAsRead(self, request): pass
+    
+    # الإعلانات
+    def getSquareChatAnnouncements(self, request): pass
+    def createSquareChatAnnouncement(self, request): pass
+    def deleteSquareChatAnnouncement(self, request): pass
 
 class Client(Iface):
-    """
-    عميل السكوير (Square Client):
-    الجزء الذي يتواصل مباشرة مع سيرفرات LINE OpenChat.
-    """
     def __init__(self, iprot, oprot=None):
         self._iprot = self._oprot = iprot
+        if oprot is not None:
+            self._oprot = oprot
         self._seqid = 0
 
-    def destroyMessage(self, request):
-        """تنفيذ أمر حذف الرسالة من السيرفر"""
-        self.send_destroyMessage(request)
-        return self.recv_destroyMessage()
+    def getSquare(self, req):
+        self.send_getSquare(req)
+        return self.recv_getSquare()
 
-    def send_destroyMessage(self, request):
-        self._oprot.writeMessageBegin('destroyMessage', TMessageType.CALL, self._seqid)
-        args = destroyMessage_args()
-        args.request = request
+    def send_getSquare(self, req):
+        self._oprot.writeMessageBegin('getSquare', TMessageType.CALL, self._seqid)
+        args = getSquare_args()
+        args.request = req
         args.write(self._oprot)
         self._oprot.writeMessageEnd()
         self._oprot.trans.flush()
+
+    def recv_getSquare(self):
+        iprot = self._iprot
+        (fname, mtype, rseqid) = iprot.readMessageBegin()
+        if mtype == TMessageType.EXCEPTION:
+            x = TApplicationException()
+            x.read(iprot)
+            iprot.readMessageEnd()
+            raise x
+        result = getSquare_result()
+        result.read(iprot)
+        iprot.readMessageEnd()
+        if result.success is not None:
+            return result.success
+        if result.e is not None:
+            raise result.e
+        raise TApplicationException(TApplicationException.MISSING_RESULT, "getSquare failed")
+
+    # تطبيق نفس النمط لباقي الدوال
+    def reportSquareMember(self, req):
+        self.send_reportSquareMember(req)
+        return self.recv_reportSquareMember()
+
+    def updateSquareAuthority(self, req):
+        self.send_updateSquareAuthority(req)
+        return self.recv_updateSquareAuthority()
+
+class Processor(Iface, TProcessor):
+    def __init__(self, handler):
+        self._handler = handler
+        self._processMap = {
+            "getSquare": Processor.process_getSquare,
+            "getJoinedSquares": Processor.process_getJoinedSquares,
+            "createSquare": Processor.process_createSquare,
+            "updateSquare": Processor.process_updateSquare,
+            "deleteSquare": Processor.process_deleteSquare,
+            "getSquareMembers": Processor.process_getSquareMembers,
+            "approveSquareMembers": Processor.process_approveSquareMembers,
+            "rejectSquareMembers": Processor.process_rejectSquareMembers,
+            "reportSquare": Processor.process_reportSquare,
+            "reportSquareMember": Processor.process_reportSquareMember,
+            "reportSquareMessage": Processor.process_reportSquareMessage,
+            "getSquareAuthority": Processor.process_getSquareAuthority,
+            "updateSquareAuthority": Processor.process_updateSquareAuthority,
+            "updateSquareMemberRelation": Processor.process_updateSquareMemberRelation,
+            "sendMessage": Processor.process_sendMessage,
+            "destroyMessage": Processor.process_destroyMessage,
+        }
+
+    def process(self, iprot, oprot):
+        (name, type, seqid) = iprot.readMessageBegin()
+        if name not in self._processMap:
+            iprot.skip(TType.STRUCT)
+            iprot.readMessageEnd()
+            x = TApplicationException(TApplicationException.UNKNOWN_METHOD, f'Unknown function {name}')
+            oprot.writeMessageBegin(name, TMessageType.EXCEPTION, seqid)
+            x.write(oprot)
+            oprot.writeMessageEnd()
+            oprot.trans.flush()
+            return
+        self._processMap[name](self, seqid, iprot, oprot)
+        return True
+
+    def process_getSquare(self, seqid, iprot, oprot):
+        args = getSquare_args()
+        args.read(iprot)
+        iprot.readMessageEnd()
+        result = getSquare_result()
+        try:
+            result.success = self._handler.getSquare(args.request)
+            msg_type = TMessageType.REPLY
+        except TTransport.TTransportException:
+            raise
+        except SquareException as e:
+            msg_type = TMessageType.REPLY
+            result.e = e
+        except Exception:
+            logging.exception('خطأ في المعالج')
+            msg_type = TMessageType.EXCEPTION
+            result = TApplicationException(TApplicationException.INTERNAL_ERROR, 'Internal error')
+        oprot.writeMessageBegin("getSquare", msg_type, seqid)
+        result.write(oprot)
+        oprot.writeMessageEnd()
+        oprot.trans.flush()
+
+# هياكل البيانات المختصرة
+class getSquare_args(object):
+    def __init__(self, request=None):
+        self.request = request
+
+class getSquare_result(object):
+    def __init__(self, success=None, e=None):
+        self.success = success
+        self.e = e
+
+fix_spec(all_structs)
+del all_structs
