2893960e02f7f62622dd8bc6c51d6f0c066dd4f8
عبير <alqaws151@gmail.com>
2025-12-21 17:12:02 +0300

    Update ChannelApplicationProvidedService.py

--- akad/ChannelApplicationProvidedService.py
+++ akad/ChannelApplicationProvidedService.py
@@ -1,56 +1,240 @@
-# -*- coding: utf-8 -*-
-# تم توليد هذا الملف بواسطة Thrift Compiler (0.11.0)
-# هذا الملف يدير الخدمات التي توفرها تطبيقات القنوات (Channel Application Provided Service)
-# تم تعريب التعليقات لشرح الوظائف المتعلقة بإدارة المشتركين وبيانات القنوات
-
-from thrift.Thrift import TType, TMessageType, TFrozenDict, TException, TApplicationException
+from thrift.Thrift import TType, TMessageType, TException, TApplicationException
 from thrift.protocol.TProtocol import TProtocolException
 from thrift.TRecursive import fix_spec
-
 import sys
 import logging
 from .ttypes import *
 from thrift.Thrift import TProcessor
 from thrift.transport import TTransport
+
 all_structs = []
 
 class Iface(object):
-    """
-    واجهة خدمات تطبيقات القناة (Interface):
-    توفر الوظائف اللازمة لإدارة المشتركين والوصول لبيانات جهات الاتصال المتقدمة.
-    """
-    def activeBuddySubscriberCount(self):
-        """جلب عدد المشتركين النشطين (Active Buddies) في القناة"""
-        pass
-
-    def addOperationForChannel(self, opType, param1, param2, param3):
-        """إضافة عملية جديدة خاصة بالقناة (مثل تحديث حالة أو إرسال تنبيه)"""
-        pass
-
-    def getContactsForChannel(self, ids):
-        """جلب بيانات جهات اتصال محددة من خلال معرفاتهم (MIDs) لصالح القناة"""
-        pass
-
-    def findContactByUseridWithoutAbuseBlockForChannel(self, userid):
-        """
-        البحث عن مستخدم بواسطة ID:
-        تتيح للقناة العثور على مستخدمين وتجاوز بعض قيود الحظر للأغراض الإدارية.
-        """
-        pass
+    # Group Management
+    def getGroupMemberMids(self, groupId): pass
+    def getGroupsForChannel(self, groupIds): pass
+    def getJoinedGroupIdsForChannel(self): pass
+    def isGroupMember(self, groupId): pass
+    
+    # Contact Management
+    def findContactByUseridWithoutAbuseBlockForChannel(self, userid): pass
+    def getAllContactIdsForChannel(self): pass
+    def getContactsForChannel(self, ids): pass
+    def getSimpleChannelContacts(self, ids): pass
+    def isInContact(self, mid): pass
+    
+    # Messaging
+    def sendMessageForChannel(self, message): pass
+    
+    # Operations
+    def addOperationForChannel(self, opType, param1, param2, param3): pass
 
 class Client(Iface):
-    """
-    العميل (Client): المسؤول عن إرسال الطلبات إلى سيرفرات LINE الخاصة بالقنوات.
-    """
     def __init__(self, iprot, oprot=None):
         self._iprot = self._oprot = iprot
-        if oprot is not None:
-            self._oprot = oprot
+        if oprot: self._oprot = oprot
         self._seqid = 0
 
-    def activeBuddySubscriberCount(self):
-        # تنفيذ طلب جلب عدد المشتركين
-        self.send_activeBuddySubscriberCount()
-        return self.recv_activeBuddySubscriberCount()
+    # Group Methods
+    def getGroupMemberMids(self, groupId):
+        self.send_getGroupMemberMids(groupId)
+        return self.recv_getGroupMemberMids()
+    
+    def send_getGroupMemberMids(self, groupId):
+        self._oprot.writeMessageBegin('getGroupMemberMids', TMessageType.CALL, self._seqid)
+        args = getGroupMemberMids_args()
+        args.groupId = groupId
+        args.write(self._oprot)
+        self._oprot.writeMessageEnd()
+        self._oprot.trans.flush()
+    
+    def recv_getGroupMemberMids(self):
+        iprot = self._iprot
+        (fname, mtype, rseqid) = iprot.readMessageBegin()
+        if mtype == TMessageType.EXCEPTION:
+            x = TApplicationException()
+            x.read(iprot)
+            iprot.readMessageEnd()
+            raise x
+        result = getGroupMemberMids_result()
+        result.read(iprot)
+        iprot.readMessageEnd()
+        if result.success is not None: return result.success
+        if result.e is not None: raise result.e
+        raise TApplicationException(TApplicationException.MISSING_RESULT, "getGroupMemberMids failed")
 
-# ... (يستمر الملف في تعريف الأكواد التقنية لمعالجة البيانات المستلمة وتسلسلها)
+    def getGroupsForChannel(self, groupIds):
+        self.send_getGroupsForChannel(groupIds)
+        return self.recv_getGroupsForChannel()
+    
+    def send_getGroupsForChannel(self, groupIds):
+        self._oprot.writeMessageBegin('getGroupsForChannel', TMessageType.CALL, self._seqid)
+        args = getGroupsForChannel_args()
+        args.groupIds = groupIds
+        args.write(self._oprot)
+        self._oprot.writeMessageEnd()
+        self._oprot.trans.flush()
+    
+    def recv_getGroupsForChannel(self):
+        iprot = self._iprot
+        (fname, mtype, rseqid) = iprot.readMessageBegin()
+        if mtype == TMessageType.EXCEPTION:
+            x = TApplicationException()
+            x.read(iprot)
+            iprot.readMessageEnd()
+            raise x
+        result = getGroupsForChannel_result()
+        result.read(iprot)
+        iprot.readMessageEnd()
+        if result.success is not None: return result.success
+        if result.e is not None: raise result.e
+        raise TApplicationException(TApplicationException.MISSING_RESULT, "getGroupsForChannel failed")
+
+    def isGroupMember(self, groupId):
+        self.send_isGroupMember(groupId)
+        return self.recv_isGroupMember()
+    
+    def send_isGroupMember(self, groupId):
+        self._oprot.writeMessageBegin('isGroupMember', TMessageType.CALL, self._seqid)
+        args = isGroupMember_args()
+        args.groupId = groupId
+        args.write(self._oprot)
+        self._oprot.writeMessageEnd()
+        self._oprot.trans.flush()
+    
+    def recv_isGroupMember(self):
+        iprot = self._iprot
+        (fname, mtype, rseqid) = iprot.readMessageBegin()
+        if mtype == TMessageType.EXCEPTION:
+            x = TApplicationException()
+            x.read(iprot)
+            iprot.readMessageEnd()
+            raise x
+        result = isGroupMember_result()
+        result.read(iprot)
+        iprot.readMessageEnd()
+        if result.success is not None: return result.success
+        if result.e is not None: raise result.e
+        raise TApplicationException(TApplicationException.MISSING_RESULT, "isGroupMember failed")
+
+    # Contact Methods
+    def isInContact(self, mid):
+        self.send_isInContact(mid)
+        return self.recv_isInContact()
+    
+    def send_isInContact(self, mid):
+        self._oprot.writeMessageBegin('isInContact', TMessageType.CALL, self._seqid)
+        args = isInContact_args()
+        args.mid = mid
+        args.write(self._oprot)
+        self._oprot.writeMessageEnd()
+        self._oprot.trans.flush()
+    
+    def recv_isInContact(self):
+        iprot = self._iprot
+        (fname, mtype, rseqid) = iprot.readMessageBegin()
+        if mtype == TMessageType.EXCEPTION:
+            x = TApplicationException()
+            x.read(iprot)
+            iprot.readMessageEnd()
+            raise x
+        result = isInContact_result()
+        result.read(iprot)
+        iprot.readMessageEnd()
+        if result.success is not None: return result.success
+        if result.e is not None: raise result.e
+        raise TApplicationException(TApplicationException.MISSING_RESULT, "isInContact failed")
+
+    # Message Method
+    def sendMessageForChannel(self, message):
+        self.send_sendMessageForChannel(message)
+        return self.recv_sendMessageForChannel()
+    
+    def send_sendMessageForChannel(self, message):
+        self._oprot.writeMessageBegin('sendMessageForChannel', TMessageType.CALL, self._seqid)
+        args = sendMessageForChannel_args()
+        args.message = message
+        args.write(self._oprot)
+        self._oprot.writeMessageEnd()
+        self._oprot.trans.flush()
+    
+    def recv_sendMessageForChannel(self):
+        iprot = self._iprot
+        (fname, mtype, rseqid) = iprot.readMessageBegin()
+        if mtype == TMessageType.EXCEPTION:
+            x = TApplicationException()
+            x.read(iprot)
+            iprot.readMessageEnd()
+            raise x
+        result = sendMessageForChannel_result()
+        result.read(iprot)
+        iprot.readMessageEnd()
+        if result.success is not None: return result.success
+        if result.e is not None: raise result.e
+        raise TApplicationException(TApplicationException.MISSING_RESULT, "sendMessageForChannel failed")
+
+    # Operation Method
+    def addOperationForChannel(self, opType, param1, param2, param3):
+        self.send_addOperationForChannel(opType, param1, param2, param3)
+        self.recv_addOperationForChannel()
+    
+    def send_addOperationForChannel(self, opType, param1, param2, param3):
+        self._oprot.writeMessageBegin('addOperationForChannel', TMessageType.CALL, self._seqid)
+        args = addOperationForChannel_args()
+        args.opType = opType
+        args.param1 = param1
+        args.param2 = param2
+        args.param3 = param3
+        args.write(self._oprot)
+        self._oprot.writeMessageEnd()
+        self._oprot.trans.flush()
+    
+    def recv_addOperationForChannel(self):
+        iprot = self._iprot
+        (fname, mtype, rseqid) = iprot.readMessageBegin()
+        if mtype == TMessageType.EXCEPTION:
+            x = TApplicationException()
+            x.read(iprot)
+            iprot.readMessageEnd()
+            raise x
+        result = addOperationForChannel_result()
+        result.read(iprot)
+        iprot.readMessageEnd()
+        if result.e is not None: raise result.e
+        return
+
+# Struct definitions (compact version - only essential fields)
+class getGroupMemberMids_args(object):
+    def __init__(self, groupId=None):
+        self.groupId = groupId
+    thrift_spec = (None, (1, TType.STRING, 'groupId', 'UTF8', None))
+
+class getGroupMemberMids_result(object):
+    def __init__(self, success=None, e=None):
+        self.success = success
+        self.e = e
+    thrift_spec = ((0, TType.LIST, 'success', (TType.STRING, 'UTF8', False), None), (1, TType.STRUCT, 'e', [TalkException, None], None))
+
+class isGroupMember_args(object):
+    def __init__(self, groupId=None):
+        self.groupId = groupId
+    thrift_spec = (None, (1, TType.STRING, 'groupId', 'UTF8', None))
+
+class isGroupMember_result(object):
+    def __init__(self, success=None, e=None):
+        self.success = success
+        self.e = e
+    thrift_spec = ((0, TType.BOOL, 'success', None, None), (1, TType.STRUCT, 'e', [TalkException, None], None))
+
+class sendMessageForChannel_args(object):
+    def __init__(self, message=None):
+        self.message = message
+    thrift_spec = (None, None, (2, TType.STRUCT, 'message', [Message, None], None))
+
+class sendMessageForChannel_result(object):
+    def __init__(self, success=None, e=None):
+        self.success = success
+        self.e = e
+    thrift_spec = ((0, TType.STRUCT, 'success', [Message, None], None), (1, TType.STRUCT, 'e', [TalkException, None], None))
+
+fix_spec(all_structs)
